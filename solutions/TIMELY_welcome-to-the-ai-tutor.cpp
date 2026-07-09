cce9fb34d9fc3d9faf764c40773231882e5d89ad
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-09 15:57:18 +0530

    Add TIMELY

---
+++ solutions/TIMELY_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,30 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: TIMELY
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+int x;
+cin >> x;
+
+cout << (x >= 30 ? "YES" : "NO") << '\n';
+}
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+int t;
+cin >> t;
+
+while (t--) {
+solve();
+}
+
+return 0;
+}
