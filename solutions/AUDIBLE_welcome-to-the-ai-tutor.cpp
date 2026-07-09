aa281fce42e74d3849ae985888e3750e0441391b
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-09 10:45:35 +0530

    Add AUDIBLE

---
+++ solutions/AUDIBLE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: AUDIBLE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X >= 67 && X <= 45000) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
