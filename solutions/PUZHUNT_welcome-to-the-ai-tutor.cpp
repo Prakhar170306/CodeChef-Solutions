ab48611888569d6da2374a0dbeece5671e10e2c3
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-09 16:05:24 +0530

    Add PUZHUNT

---
+++ solutions/PUZHUNT_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: PUZHUNT
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+int n;
+cin >> n;
+
+cout << (n >= 6 && n <= 8 ? "Yes" : "No") << '\n';
+
+return 0;
+}
