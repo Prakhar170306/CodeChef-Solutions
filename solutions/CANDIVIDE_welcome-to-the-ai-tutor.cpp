2667ad75cfbbd8fef1c193be08fa7e3b546363da
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 00:43:21 +0530

    Add CANDIVIDE

---
+++ solutions/CANDIVIDE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CANDIVIDE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CANDIVIDE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+if (N % 3 == 0) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
