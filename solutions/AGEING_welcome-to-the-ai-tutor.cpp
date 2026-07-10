2ee4552e93cc4a624574b338235be762d7591c66
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 01:11:26 +0530

    Add AGEING

---
+++ solutions/AGEING_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: AGEING
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AGEING
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+int chefinaAge = X - 10;
+
+cout << chefinaAge << endl;
+}
+
+return 0;
+}
