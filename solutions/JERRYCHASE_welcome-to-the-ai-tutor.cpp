384499782335c96ae94fa9308278f656aec27ce2
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 01:08:45 +0530

    Add JERRYCHASE

---
+++ solutions/JERRYCHASE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: JERRYCHASE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+if (Y > X) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
