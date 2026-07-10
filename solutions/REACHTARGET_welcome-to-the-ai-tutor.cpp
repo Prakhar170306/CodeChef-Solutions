e6fac09770e5e1473af2625232f923732e35340a
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 00:22:07 +0530

    Add REACHTARGET

---
+++ solutions/REACHTARGET_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: REACHTARGET
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACHTARGET
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+int t;
+cin >> t;
+
+while (t--) {
+int x, y;
+cin >> x >> y;
+
+cout << x - y << '\n';
+}
+
+return 0;
+}
