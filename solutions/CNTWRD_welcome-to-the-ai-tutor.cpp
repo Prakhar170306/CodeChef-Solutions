5749315de39ecbc2edc447c8ed1502cdeaf323c9
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 01:01:42 +0530

    Add CNTWRD

---
+++ solutions/CNTWRD_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CNTWRD
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N, M;
+cin >> N >> M;
+
+int total = N * M;
+
+cout << total << endl;
+}
+
+return 0;
+}
