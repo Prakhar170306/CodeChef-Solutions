71cb5ab5bba707d69a85b4cf857ed18756a61309
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 00:51:31 +0530

    Add PAR2

---
+++ solutions/PAR2_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: PAR2
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+if (N % 2 == 0) {
+cout << "Yes" << endl;
+} else {
+cout << "No" << endl;
+}
+}
+
+return 0;
+}
