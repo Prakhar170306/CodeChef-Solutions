5424b70042d846e3fa563f906e3bb630cdddd87c
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 01:04:25 +0530

    Add BTRYHLTH

---
+++ solutions/BTRYHLTH_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: BTRYHLTH
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X >= 80) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
