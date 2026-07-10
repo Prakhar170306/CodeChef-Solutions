3ed743bcbf58c6dfb0835cccf8fd2294be5a0847
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 00:28:05 +0530

    Add RIP2000

---
+++ solutions/RIP2000_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: RIP2000
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int N;
+cin >> N;
+
+int ans = N * 4;
+
+cout << ans;
+
+return 0;
+}
