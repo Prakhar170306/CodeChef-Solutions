0df0c3cb9f61cc311595ca615d4d2a0815475576
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 00:49:19 +0530

    Add CHEFONDATE

---
+++ solutions/CHEFONDATE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CHEFONDATE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHEFONDATE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+if (X >= Y) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
