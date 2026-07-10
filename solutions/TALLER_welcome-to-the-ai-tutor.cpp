8c3dccf47d8179802da9e096024c5130fedb3bad
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-10 21:56:43 +0530

    Add TALLER

---
+++ solutions/TALLER_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: TALLER
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TALLER
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+if (X > Y) {
+cout << "A" << endl;
+} else {
+cout << "B" << endl;
+}
+}
+
+return 0;
+}
