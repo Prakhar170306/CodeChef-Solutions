67cb92648dd5e1ee9771d8ce5b293874e1633f44
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 01:00:01 +0530

    Add PRIZEPOOL

---
+++ solutions/PRIZEPOOL_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: PRIZEPOOL
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+int total = (10 * X) + (90 * Y);
+
+cout << total << endl;
+}
+
+return 0;
+}
