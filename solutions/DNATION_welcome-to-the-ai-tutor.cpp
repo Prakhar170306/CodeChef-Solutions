71a588beba8cf70862134f797acf75af473c3a3a
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 12:33:07 +0530

    Add DNATION

---
+++ solutions/DNATION_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: DNATION
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DNATION
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+int donation = Y - X;
+
+cout << donation << endl;
+}
+
+return 0;
+}
