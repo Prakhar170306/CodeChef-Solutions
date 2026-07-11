4020471e74a6a31f8ad5ca1b7eae0423e8efbbf7
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 02:17:48 +0530

    Add ONEMORE

---
+++ solutions/ONEMORE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: ONEMORE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ONEMORE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X > 24) {
+cout << "YES\n";
+} else {
+cout << "NO\n";
+}
+}
+
+return 0;
+}
