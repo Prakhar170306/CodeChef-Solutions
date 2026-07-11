dda5b8c116efb7e1dad5d8d631598185f079f7c2
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 02:52:20 +0530

    Add FEVER

---
+++ solutions/FEVER_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: FEVER
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FEVER
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X > 98) {
+cout << "YES\n";
+} else {
+cout << "NO\n";
+}
+}
+
+return 0;
+}
