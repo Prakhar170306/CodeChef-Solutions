c782c8e60628e01e97c8407e9f463f0be98f7034
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 02:15:15 +0530

    Add WAITTIME

---
+++ solutions/WAITTIME_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: WAITTIME
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WAITTIME
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int K, X;
+cin >> K >> X;
+
+cout << (K * 7) - X << endl;
+}
+
+return 0;
+}
