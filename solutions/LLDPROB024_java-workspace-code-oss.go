1cf705357a5d4549790622016896b4ab36347a58
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 13:43:04 +0530

    Add LLDPROB024

---
+++ solutions/LLDPROB024_java-workspace-code-oss.go
@@ -0,0 +1,17 @@
+/*
+Problem: .java - workspace - Code - OSS
+Problem Code: LLDPROB024
+Platform: CodeChef
+URL: https://www.codechef.com/skill-test/DEVWEEKEND58/problems/LLDPROB024?folder=%2Fhome%2Fchef%2Fworkspace&tab=tests
+*/
+
+--- Welcome to the LLD Coffee Shop ---
+
+Order 1: Espresso
+Total: $2.00
+
+Order 2: House Latte, Soy Milk, Caramel Syrup
+Total: $4.75
+
+Order 3: Espresso, Extra Shot, Extra Shot, Caramel Syrup
+Total: $5.15
