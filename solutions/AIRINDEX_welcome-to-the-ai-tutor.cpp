49e0fc81300834e50cc1ea2a58d878b6c0686e96
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 02:48:39 +0530

    Add AIRINDEX

---
+++ solutions/AIRINDEX_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: AIRINDEX
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int X;
+cin >> X;
+
+if (X < 100) {
+cout << "YES";
+} else {
+cout << "NO";
+}
+
+return 0;
+}
