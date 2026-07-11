7e2d1bdbc373dbca1bcd39918e8c57d9374125d2
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 02:37:39 +0530

    Add CHESSTIME

---
+++ solutions/CHESSTIME_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CHESSTIME
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHESSTIME
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+cout << N * 3 << '\n';
+}
+
+return 0;
+}
