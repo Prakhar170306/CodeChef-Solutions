d2cdcdb072e2a0d15b3c0907a46fef369b216d92
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 03:00:26 +0530

    Add SLEEP

---
+++ solutions/SLEEP_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: SLEEP
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SLEEP
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X < 7) {
+cout << "YES\n";
+} else {
+cout << "NO\n";
+}
+}
+
+return 0;
+}
