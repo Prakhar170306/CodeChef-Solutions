29dee136a44fb2c8a9f6d41fd28ad71c7a58cf32
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 02:44:51 +0530

    Add COUGAME

---
+++ solutions/COUGAME_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: COUGAME
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COUGAME
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int G, B;
+cin >> G >> B;
+
+cout << B - G << '\n';
+}
+
+return 0;
+}
