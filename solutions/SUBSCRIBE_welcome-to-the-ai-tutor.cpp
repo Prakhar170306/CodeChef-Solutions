542020e2dd5be1ee8c086b377370e0af205e9857
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-11 12:48:48 +0530

    Add SUBSCRIBE

---
+++ solutions/SUBSCRIBE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: SUBSCRIBE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SUBSCRIBE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X > 30) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
