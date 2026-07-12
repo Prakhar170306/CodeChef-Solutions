665cf436dc92a854e5d8c847e42e0a1a5c83a567
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 00:37:48 +0530

    Add DISCNT

---
+++ solutions/DISCNT_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: DISCNT
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int x;
+cin >> x;
+
+cout << 100 - x << endl;
+}
+
+return 0;
+}
