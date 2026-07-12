2c1b69512a458de317b6468ae8a39ed63706e809
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 00:34:27 +0530

    Add TFPAPER

---
+++ solutions/TFPAPER_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: TFPAPER
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TFPAPER
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N, K;
+cin >> N >> K;
+
+cout << N - K << endl;
+}
+
+return 0;
+}
