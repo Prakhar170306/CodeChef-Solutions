4b7b21c7d9f2e3bbf183c296e4f172345d2acb3b
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 11:22:43 +0530

    Add WATERREQ

---
+++ solutions/WATERREQ_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: WATERREQ
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+cout << 2 * N << endl;
+}
+
+return 0;
+}
+return keyword
