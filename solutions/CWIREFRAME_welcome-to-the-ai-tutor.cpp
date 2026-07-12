205272745e5758201d1644a232957cf106027cad
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 15:52:22 +0530

    Add CWIREFRAME

---
+++ solutions/CWIREFRAME_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CWIREFRAME
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWIREFRAME
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N, M, X;
+cin >> N >> M >> X;
+
+cout << 2 * (N + M) * X << endl;
+}
+
+return 0;
+}
