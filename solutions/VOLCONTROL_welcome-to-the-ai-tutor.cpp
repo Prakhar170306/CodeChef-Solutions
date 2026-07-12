a1d00aa09dac14fe8ac34ae7ccf5ab9caedfa108
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 00:56:23 +0530

    Add VOLCONTROL

---
+++ solutions/VOLCONTROL_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: VOLCONTROL
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/VOLCONTROL
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+cout << abs(X - Y) << endl;
+}
+
+return 0;
+}
