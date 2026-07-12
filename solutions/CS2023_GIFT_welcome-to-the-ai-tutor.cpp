eac01b8f41800815f6aef3d6cb1058d51448560f
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 23:12:39 +0530

    Add CS2023_GIFT

---
+++ solutions/CS2023_GIFT_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CS2023_GIFT
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int X, N, M;
+cin >> X >> N >> M;
+
+if (X + M >= N) {
+cout << "YES";
+} else {
+cout << "NO";
+}
+
+return 0;
+}
