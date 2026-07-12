d8a528f92dfddafcc3bc2c7064fbd377d6ae6818
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 16:03:22 +0530

    Add MINCOINSREQ

---
+++ solutions/MINCOINSREQ_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: MINCOINSREQ
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+cout << X % 10 << endl;
+}
+
+return 0;
+}
