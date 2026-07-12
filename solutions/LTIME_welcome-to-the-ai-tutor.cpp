5810a0d4a9bce2c4f35aac6a378fb0ad62eb5bb8
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 11:24:38 +0530

    Add LTIME

---
+++ solutions/LTIME_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: LTIME
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X >= 1 && X <= 4) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
