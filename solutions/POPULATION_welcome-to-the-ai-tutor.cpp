80211f1471aa49aeafb320bc83f5dbf254abbbd1
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 11:29:29 +0530

    Add POPULATION

---
+++ solutions/POPULATION_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: POPULATION
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y, Z;
+cin >> X >> Y >> Z;
+
+cout << X - Y + Z << endl;
+}
+
+return 0;
+}
