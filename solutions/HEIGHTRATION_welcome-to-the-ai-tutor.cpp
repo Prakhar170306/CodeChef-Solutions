56ce7504b9966a585e38d6b4ea3d5e1554e1e82e
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 00:39:30 +0530

    Add HEIGHTRATION

---
+++ solutions/HEIGHTRATION_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,18 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: HEIGHTRATION
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int a, b;
+cin >> a >> b;
+
+cout << max(a, b);
+
+return 0;
+}
