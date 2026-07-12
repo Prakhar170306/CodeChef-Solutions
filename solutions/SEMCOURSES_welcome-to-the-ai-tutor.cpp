899fb4d8660542ea22911693b5067144651bbf0f
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-12 11:17:44 +0530

    Add SEMCOURSES

---
+++ solutions/SEMCOURSES_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: SEMCOURSES
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y, Z;
+cin >> X >> Y >> Z;
+
+cout << X * Y * Z << endl;
+}
+
+return 0;
+}
