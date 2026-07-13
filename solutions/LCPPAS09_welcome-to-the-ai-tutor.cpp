07f5d8f793576a7b35187671a7fd25d135c72d27
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 15:06:37 +0530

    Add LCPPAS09

---
+++ solutions/LCPPAS09_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,16 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: LCPPAS09
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/cpp/LPCPAS01/problems/LCPPAS09
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// Write your code below
+cout<<9*12;
+
+return 0;
+}
