8f7c049c314fdf432de3073123b85b40f98eea3a
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 15:07:40 +0530

    Add LCPPAS151

---
+++ solutions/LCPPAS151_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,19 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: LCPPAS151
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/cpp/LPCPAS02/problems/LCPPAS151
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// Write your code here
+cout<<8+4<<endl;
+cout<<8-4<<endl;
+cout<<8/4<<endl;
+cout<<8*4<<endl;
+
+return 0;
+}
