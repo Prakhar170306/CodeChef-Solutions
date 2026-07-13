bae70648f5990dc9d9094fae57b2c155e2787698
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 15:07:28 +0530

    Add LCPPAS20

---
+++ solutions/LCPPAS20_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: LCPPAS20
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/cpp/LPCPAS02/problems/LCPPAS20
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// your code goes here
+cout << 1 << " - " << 1 * 1 << endl;
+cout << 2 << " - " << 2 * 2 << endl;
+cout << 3 << " - " << 3 * 3 << endl;
+cout << 4 << " - " << 4 * 4 << endl;
+cout << 5 << " - " << 5 * 5 << endl;
+return 0;
+
+}
