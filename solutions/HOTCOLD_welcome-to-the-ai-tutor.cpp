c4d84fb4622bb0564910c41e84c38b731ea594a7
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-14 00:46:36 +0530

    Add HOTCOLD

---
+++ solutions/HOTCOLD_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: HOTCOLD
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int C;
+cin >> C;
+
+if (C > 20) {
+cout << "HOT" << endl;
+} else {
+cout << "COLD" << endl;
+}
+}
+
+return 0;
+}
