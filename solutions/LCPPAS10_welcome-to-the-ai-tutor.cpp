47c9914c77a39b2e3b52c71fb63b892b36a3db68
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 15:06:56 +0530

    Add LCPPAS10

---
+++ solutions/LCPPAS10_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,15 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: LCPPAS10
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/cpp/LPCPAS01/problems/LCPPAS10
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// your code goes here
+cout<<"Learn Coding on CodeChef";
+return 0;
+}
