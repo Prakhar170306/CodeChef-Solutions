671720e91c89ce1f24c73455825d60ca1fe20961
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-13 15:07:22 +0530

    Add LCPPAS19

---
+++ solutions/LCPPAS19_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,15 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: LCPPAS19
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/cpp/LPCPAS02/problems/LCPPAS19
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// Your code here
+cout<<"I"<<endl<<"love"<<endl<<"CodeChef";
+return 0;
+}
