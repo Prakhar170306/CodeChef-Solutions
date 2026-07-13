d4b3cc0f90fc643f1162ac8cbac2edd047722e65
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-14 00:54:51 +0530

    Add FBC

---
+++ solutions/FBC_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: FBC
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FBC
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int K, X;
+cin >> K >> X;
+
+cout << K - X << endl;
+}
+
+return 0;
+}
