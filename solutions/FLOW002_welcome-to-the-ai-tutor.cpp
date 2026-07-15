2d4268534558909bf9fce53b6c1c0e0710b123b3
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-15 13:09:02 +0530

    Add FLOW002

---
+++ solutions/FLOW002_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: FLOW002
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int A, B;
+cin >> A >> B;
+
+cout << A % B << endl;
+}
+
+return 0;
+}
