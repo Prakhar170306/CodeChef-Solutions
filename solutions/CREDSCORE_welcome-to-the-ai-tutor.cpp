c930ccf44fa99c97aefdfd673196396d4941eddb
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-16 01:41:48 +0530

    Add CREDSCORE

---
+++ solutions/CREDSCORE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CREDSCORE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CREDSCORE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int X;
+cin >> X;
+
+if (X >= 750) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+
+return 0;
+}
