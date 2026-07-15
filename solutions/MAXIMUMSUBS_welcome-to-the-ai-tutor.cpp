5a3f05439c14f38611eb89b30e03752645d3390b
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-15 13:33:23 +0530

    Add MAXIMUMSUBS

---
+++ solutions/MAXIMUMSUBS_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: MAXIMUMSUBS
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MAXIMUMSUBS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+cout << 2 * X << endl;
+}
+
+return 0;
+}
