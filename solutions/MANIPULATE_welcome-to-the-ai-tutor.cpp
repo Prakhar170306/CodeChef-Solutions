484b653e12d6801bac73f06bab33f1001fd5be11
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-15 13:19:16 +0530

    Add MANIPULATE

---
+++ solutions/MANIPULATE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,27 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: MANIPULATE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANIPULATE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+if (X >= Y) {
+cout << "YES" << endl;
+} else {
+cout << "NO" << endl;
+}
+}
+
+return 0;
+}
