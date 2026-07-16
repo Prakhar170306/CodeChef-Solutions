264e2233c0ef43ed18a48953c3bf182e48197083
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-17 02:37:40 +0530

    Add INSURANCE

---
+++ solutions/INSURANCE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: INSURANCE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INSURANCE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin >> t;
+
+while (t--) {
+int x, y;
+cin >> x >> y;
+
+cout << min(x, y) << endl;
+}
+
+return 0;
+}
