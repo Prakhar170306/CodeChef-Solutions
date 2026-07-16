186d179699118e8bbaef13d3bf1f0d23712a6c02
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-17 02:34:13 +0530

    Add PRACTICEPERF

---
+++ solutions/PRACTICEPERF_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: PRACTICEPERF
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACTICEPERF
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int p1, p2, p3, p4;
+cin >> p1 >> p2 >> p3 >> p4;
+
+int count = 0;
+
+if (p1 >= 10) count++;
+if (p2 >= 10) count++;
+if (p3 >= 10) count++;
+if (p4 >= 10) count++;
+
+cout << count << endl;
+
+return 0;
+}
