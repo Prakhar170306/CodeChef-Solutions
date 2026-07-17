083d6f0aa2c583ad1fa970a9e88ad108629ee6d3
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-18 01:18:07 +0530

    Add SONGS

---
+++ solutions/SONGS_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: SONGS
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SONGS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N, X;
+cin >> N >> X;
+
+cout << N / (3 * X) << endl;
+}
+
+return 0;
+}
