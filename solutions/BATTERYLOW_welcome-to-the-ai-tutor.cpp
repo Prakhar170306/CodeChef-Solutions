f5ae8cb71fe70a789108003f5fcd7f27afdbee18
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-18 00:11:39 +0530

    Add BATTERYLOW

---
+++ solutions/BATTERYLOW_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: BATTERYLOW
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BATTERYLOW
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin >> t;
+
+while (t--) {
+int x;
+cin >> x;
+
+if (x <= 15)
+cout << "Yes" << endl;
+else
+cout << "No" << endl;
+}
+
+return 0;
+}
