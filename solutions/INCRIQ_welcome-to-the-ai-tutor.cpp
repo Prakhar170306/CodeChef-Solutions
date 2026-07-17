0a4b36a9bcbfbada743dc39bed1d89fb9dfda189
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-18 00:10:32 +0530

    Add INCRIQ

---
+++ solutions/INCRIQ_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: INCRIQ
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INCRIQ
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int x;
+cin >> x;
+
+if (x + 7 > 170)
+cout << "Yes";
+else
+cout << "No";
+
+return 0;
+}
