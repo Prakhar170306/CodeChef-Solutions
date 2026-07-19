641db04f9e4633f81aadffc39769ff4dab6d4763
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-19 23:10:49 +0530

    Add OFFICE

---
+++ solutions/OFFICE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: OFFICE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+cout << 4 * X + Y << endl;
+}
+
+return 0;
+}
