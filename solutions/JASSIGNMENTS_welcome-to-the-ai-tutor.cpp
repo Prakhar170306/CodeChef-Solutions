3056e5e9508b5d6f6b5ffb803e0ae66904481cdd
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-19 23:06:53 +0530

    Add JASSIGNMENTS

---
+++ solutions/JASSIGNMENTS_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: JASSIGNMENTS
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X + 3 <= 10)
+cout << "Yes" << endl;
+else
+cout << "No" << endl;
+}
+
+return 0;
+}
