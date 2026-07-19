b0230c5d5046071554ca3f62b93ba39e0bf74fa8
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-19 20:03:43 +0530

    Add CGYM

---
+++ solutions/CGYM_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,28 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CGYM
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CGYM
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y, Z;
+cin >> X >> Y >> Z;
+
+if (X + Y <= Z)
+cout << 2 << endl;
+else if (X <= Z)
+cout << 1 << endl;
+else
+cout << 0 << endl;
+}
+
+return 0;
+}
