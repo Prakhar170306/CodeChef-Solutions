c889d0f00d31c0f6ab24b33ecdc3881f76cd0d69
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-20 11:57:20 +0530

    Add CREDCOINS

---
+++ solutions/CREDCOINS_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: CREDCOINS
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CREDCOINS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+cout << (X * Y) / 100 << endl;
+}
+
+return 0;
+}
