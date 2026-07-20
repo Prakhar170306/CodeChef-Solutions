91daac27aae5109bef2869b645479fa04e10a2ca
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-20 18:29:53 +0530

    Add NOTEBOOK

---
+++ solutions/NOTEBOOK_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: NOTEBOOK
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NOTEBOOK
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+cout << 15 * N << endl;
+}
+
+return 0;
+}
