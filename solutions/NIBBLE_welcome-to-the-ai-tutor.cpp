9142bdc9bf37916b91358df27d9bbea2233ea989
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-22 01:18:14 +0530

    Add NIBBLE

---
+++ solutions/NIBBLE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: NIBBLE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NIBBLE
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+if (N % 4 == 0)
+cout << "Good" << endl;
+else
+cout << "Not Good" << endl;
+}
+
+return 0;
+}
