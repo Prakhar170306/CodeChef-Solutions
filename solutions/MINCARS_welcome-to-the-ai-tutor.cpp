d06a7f7c9727156866307eaafbc22070ec4eaff0
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-22 01:25:00 +0530

    Add MINCARS

---
+++ solutions/MINCARS_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: MINCARS
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MINCARS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+cout << (N + 3) / 4 << endl;
+}
+
+return 0;
+}
