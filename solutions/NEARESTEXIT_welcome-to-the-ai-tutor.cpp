a8bc4a12e8983fc88b925a80ca4eff16f92de212
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-21 23:35:26 +0530

    Add NEARESTEXIT

---
+++ solutions/NEARESTEXIT_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: NEARESTEXIT
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NEARESTEXIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X;
+cin >> X;
+
+if (X <= 50)
+cout << "LEFT" << endl;
+else
+cout << "RIGHT" << endl;
+}
+
+return 0;
+}
