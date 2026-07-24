1806437c0203fe90b17a32549982a4ecbeaeab73
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-24 22:42:03 +0530

    Add MOVIE2X

---
+++ solutions/MOVIE2X_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,18 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: MOVIE2X
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int X, Y;
+cin >> X >> Y;
+
+cout << (Y / 2) + (X - Y);
+
+return 0;
+}
