80036b592c2db4c5789c5cf48ae776934377d34f
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-25 00:21:40 +0530

    Add BATH

---
+++ solutions/BATH_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: BATH
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/BATH
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+cout << X / (2 * Y) << endl;
+}
+
+return 0;
+}
