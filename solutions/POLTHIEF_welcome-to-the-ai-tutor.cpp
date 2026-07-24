606aaf4d8c9993ea559a2a671f5b4289579f941a
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-24 22:43:22 +0530

    Add POLTHIEF

---
+++ solutions/POLTHIEF_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: POLTHIEF
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/POLTHIEF
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+cout << abs(X - Y) << endl;
+}
+
+return 0;
+}
