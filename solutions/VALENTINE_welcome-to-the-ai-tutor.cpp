8421aa0e843e21300c59f0822834d2103bdbe9fe
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-29 00:19:15 +0530

    Add VALENTINE

---
+++ solutions/VALENTINE_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: VALENTINE
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/VALENTINE
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int X, Y;
+cin >> X >> Y;
+
+cout << X / Y << endl;
+}
+
+return 0;
+}
