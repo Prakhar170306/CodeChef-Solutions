fdbfbbaaff2396d1c22e0f02b67be75d6b9769b1
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-28 22:37:44 +0530

    Add FSQRT

---
+++ solutions/FSQRT_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: FSQRT
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FSQRT
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+cout << (int)sqrt(N) << endl;
+}
+
+return 0;
+}
