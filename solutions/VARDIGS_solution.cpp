651add29239076fead795dcfb074e44189c20571
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-07-29 20:09:12 +0530

    Add VARDIGS

---
+++ solutions/VARDIGS_solution.cpp
@@ -0,0 +1,16 @@
+/*
+Problem: CodeChef Solution
+Problem Code: VARDIGS
+Platform: CodeChef
+URL: https://www.codechef.com/START249D/problems/VARDIGS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int X;
+cin >> X;
+cout << ((X / 10 != X % 10) ? "Yes" : "No");
+return 0;
+}
