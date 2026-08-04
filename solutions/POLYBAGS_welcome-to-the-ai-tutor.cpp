3efdb384245fcd8dc5d6f39d95c3e21be0e6d7ed
Prakhar Saulakhe <prakharsaulakhe71@gmail.com>
2026-08-04 22:12:47 +0530

    Add POLYBAGS

---
+++ solutions/POLYBAGS_welcome-to-the-ai-tutor.cpp
@@ -0,0 +1,35 @@
+/*
+Problem: Welcome to the  AI Tutor
+Problem Code: POLYBAGS
+Platform: CodeChef
+URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/POLYBAGS
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+int T;
+cin >> T;
+
+while (T--) {
+int N;
+cin >> N;
+
+cout << (N + 9) / 10 << '\n';
+}
+
+return 0;
+}
+make_heap snippet
+map snippet
+max snippet
+max_element snippet
+merge snippet
+mfun snippet
+min snippet
+min_element snippet
+mmap snippet
